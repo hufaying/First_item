274d05a4a7de8dd277b29a22cd390831ea619323
hufaying <fayinghu@163.com>
2022-06-18 21:35:14 +0800

    gg

--- src/com/faying/controller/HelloWorld.java
+++ src/com/faying/controller/HelloWorld.java
@@ -3,7 +3,7 @@ package com.faying.controller;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("xxxxx"); String a="";String b="";
+		System.out.println("xxxxx,{yyy}"); String a="";String b="";
 		System.out.println(">>>>>");
 	}
 	private void cc() {
